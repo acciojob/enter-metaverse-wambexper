f91d0e1f489f963e7ded72f009a5dc21d485a434
PETER WAMBEDDE <wambexpeter@gmail.com>
2023-11-06 16:57:58 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+//your JS code here. If required.
+const button = document.getElementById("enterBtn");
+const para = document.getElementById("status");
+
+button.addEventListener("click", () => {
+	para.innerHTML = "<h1>Entered Metaverse</h1>";
+})
+
+
 
